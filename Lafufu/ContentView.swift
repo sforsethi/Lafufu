20ff74ba6695e9b9e16575e245c66988e1018380
raghav-latte <131696988+raghav-latte@users.noreply.github.com>
2025-06-21 12:55:19 +0530

    Initial Commit

---
+++ Lafufu/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lafufu
+//
+//  Created by Raghav Sethi on 21/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lafufu/LafufuApp.swift
@@ -0,0 +1,17 @@
+//
+//  LafufuApp.swift
+//  Lafufu
+//
+//  Created by Raghav Sethi on 21/06/25.
+//
+
+import SwiftUI
+
+@main
+struct LafufuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
